031fda9c73cecda41956a64c1142a318cf4e0c25
jsyoo <sumnjc@gmail.com>
2020-11-24 18:00:17 +0900

    쓰레드로 분할하여 합하기

--- example/modoocpp/15_thread/divide_sum.cpp
+++ example/modoocpp/15_thread/divide_sum.cpp
@@ -4,8 +4,10 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <time.h>
 
 #define MAX_CNT 100000000
+#define THREAD_CNT 2
 
 using std::thread;
 using std::vector;
@@ -27,7 +29,30 @@ int main(){
         iVector[i] = i;
     }
 
-    unsigned int result;
-    worker(iVector.begin(), iVector.end(), &result);
+    unsigned int total = 0;
+    clock_t start = clock();
+#if 0
+    worker(iVector.begin(), iVector.end(), &total);
+    std::cout << "전체 합: " << total << std::endl;
+#else
+    vector<unsigned int> partial_sums(THREAD_CNT);
+
+    vector<thread> workers;
+    for(int i=0; i<THREAD_CNT; i++){
+        workers.push_back(thread(worker, iVector.begin()+i*(MAX_CNT/THREAD_CNT),
+                                iVector.begin()+(i+1)*(MAX_CNT/THREAD_CNT), &partial_sums[i]));
+    }
+    for (int i=0; i<THREAD_CNT; i++){
+        workers[i].join();
+    }
+
+    for (int i=0; i<THREAD_CNT; i++){
+        total += partial_sums[i];
+    }
+    std::cout << "전체 합: " << total << std::endl;
+#endif
+    clock_t end = clock();
+
+    std::cout << "elapsed time : " << (double)(end-start)/CLOCKS_PER_SEC << std::endl;
     return 0;
 }
